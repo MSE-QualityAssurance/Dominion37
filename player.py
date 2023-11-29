a5aa178c4d7a225ead56f86a1a2d98d4dccc8b37
whimsicalwill <knipewilliam20@gmail.com>
2023-11-29 12:06:11 -0500

    Add player implementation

--- player.py
+++ player.py
@@ -6,29 +6,53 @@ class Player:
         self.deck = Deck()
         self.hand = []
         self.discard_pile = []
+        self.coins = 0  # Track the number of coins a player has
+        self.actions = 1  # Track the number of actions a player can perform
+
+    def draw_card(self):
+        # Draw a single card from the deck
+        if len(self.deck.cards) == 0:
+            self.deck.reshuffle_from_discard(self.discard_pile)
+        card = self.deck.draw()
+        if card:
+            self.hand.append(card)
 
     def draw_hand(self, num_cards=5):
-        # Draw cards from the deck
+        # Draw a number of cards from the deck
         for _ in range(num_cards):
-            if len(self.deck.cards) == 0:
-                self.deck.reshuffle_from_discard(self.discard_pile)
-            card = self.deck.draw()
-            if card:
-                self.hand.append(card)
+            self.draw_card()
+
+    def add_coins(self, amount):
+        # Add coins to the player's total
+        self.coins += amount
+
+    def add_actions(self, amount):
+        # Add actions to the player's total
+        self.actions += amount
 
     def play_card(self, card):
         # Play a card from hand
         if card in self.hand:
             self.hand.remove(card)
-            # Implement card effect here
-            # For now, just discard the card
+            card.play(self)  # The card's play method is called here
             self.discard_pile.append(card)
         else:
             print("Card not in hand")
 
     def buy_card(self, card, supply):
         # Buy a card from the supply
-        if supply.buy(card.name):
-            self.discard_pile.append(card)
+        purchased_card = supply.buy(card.name)
+        if purchased_card:
+            self.discard_pile.append(purchased_card)
         else:
             print("Card not available for purchase")
+
+    def discard_hand(self):
+        # Discard the current hand to the discard pile
+        self.discard_pile.extend(self.hand)
+        self.hand.clear()
+
+    def reshuffle_discard_into_deck(self):
+        # Reshuffle discard pile into the deck
+        self.deck.reshuffle_from_discard(self.discard_pile)
+        self.discard_pile.clear()
