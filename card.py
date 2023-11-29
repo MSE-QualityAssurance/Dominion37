d02bd941b710bae755d70eed7cae4cbdf4d0742f
whimsicalwill <knipewilliam20@gmail.com>
2023-11-29 12:03:49 -0500

    Add card implementations

--- card.py
+++ card.py
@@ -10,10 +10,71 @@ class ActionCard(Card):
         self.effects = effects
 
     def play(self, player, game):
-        # Implement the effects of the card
+        for effect in self.effects:
+            effect(player, game)
+
+# Example Action Cards
+class Smithy(ActionCard):
+    def __init__(self):
+        super().__init__("Smithy", 4, [self.draw_cards])
+
+    @staticmethod
+    def draw_cards(player, game):
+        for _ in range(3):
+            player.draw_card()
+
+class Village(ActionCard):
+    def __init__(self):
+        super().__init__("Village", 3, [self.draw_card, self.add_actions])
+
+    @staticmethod
+    def draw_card(player, game):
+        player.draw_card()
+
+    @staticmethod
+    def add_actions(player, game):
+        player.add_actions(2)
 
 class TreasureCard(Card):
-    # Implementation for Treasure cards
+    def __init__(self, name, cost, value):
+        super().__init__(name, cost, "Treasure")
+        self.value = value
+
+    def play(self, player, game):
+        player.add_coins(self.value)
+
+# Example Treasure Cards
+class Copper(TreasureCard):
+    def __init__(self):
+        super().__init__("Copper", 0, 1)
+
+class Silver(TreasureCard):
+    def __init__(self):
+        super().__init__("Silver", 3, 2)
+
+class Gold(TreasureCard):
+    def __init__(self):
+        super().__init__("Gold", 6, 3)
 
 class VictoryCard(Card):
-    # Implementation for Victory cards
+    def __init__(self, name, cost, points):
+        super().__init__(name, cost, "Victory")
+        self.points = points
+
+    def play(self, player, game):
+        # Victory cards usually don't have an in-game effect
+        pass
+
+# Example Victory Cards
+class Estate(VictoryCard):
+    def __init__(self):
+        super().__init__("Estate", 2, 1)
+
+class Duchy(VictoryCard):
+    def __init__(self):
+        super().__init__("Duchy", 5, 3)
+
+class Province(VictoryCard):
+    def __init__(self):
+        super().__init__("Province", 8, 6)
+
