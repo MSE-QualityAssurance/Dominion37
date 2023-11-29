740ef82573f9f53d40de95d76b65e741f067eee2
whimsicalwill <knipewilliam20@gmail.com>
2023-11-29 12:09:34 -0500

    Changes for deck/util

--- deck.py
+++ deck.py
@@ -1,10 +1,22 @@
+import random
+
 class Deck:
     def __init__(self):
         self.cards = []
         self.discard_pile = []
 
-    def draw(self, num_cards):
-        # Draw cards from the deck
+    def draw(self):
+        # Draw a single card from the deck
+        if len(self.cards) > 0:
+            return self.cards.pop(0)
+        return None
+
+    def reshuffle_from_discard(self, discard_pile):
+        # Add discard pile to deck and shuffle
+        self.cards.extend(discard_pile)
+        random.shuffle(self.cards)
+        discard_pile.clear()
 
     def shuffle(self):
         # Shuffle the deck
+        random.shuffle(self.cards)
--- util.py
+++ util.py
@@ -1,5 +1,17 @@
-def setup_supply():
+from card import VictoryCard
+
+def setup_supply(card_list):
     # Set up the initial supply of cards
+    # card_list should be a list of tuples (Card, count)
+    supply = {}
+    for card, count in card_list:
+        supply[card.name] = (card, count)
+    return supply
 
 def calculate_victory_points(player):
     # Calculate the victory points for a player
+    victory_points = 0
+    for card in player.deck.cards + player.discard_pile:
+        if isinstance(card, VictoryCard):
+            victory_points += card.points
+    return victory_points
